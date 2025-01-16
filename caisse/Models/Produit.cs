7ced4571600c1923860e0e6f2c5006708b978120
sibeldemirel <sibeldmrl19@gamil.com>
2025-01-16 14:21:09 +0100

    Ajoutez des fichiers projet.

---
+++ caisse/Models/Produit.cs
@@ -0,0 +1,14 @@
+﻿namespace caisse.Models
+{
+    public class Produit
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int InStockQtte { get; set; }
+        public string Cetgory { get; set; }
+        public string Image { get; set; }
+        
+    }
+}
